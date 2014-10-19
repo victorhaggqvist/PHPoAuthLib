fb97692ce6580fce9d138e9bbf5fabfe9ea0262e
Victor Häggqvist <victor@snilius.com>
2014-10-19 02:34:26 +0200

    send all the things with curl

--- src/OAuth/Common/Http/Client/StreamClient.php
+++ src/OAuth/Common/Http/Client/StreamClient.php
@@ -10,95 +10,89 @@ use OAuth\Common\Http\Uri\UriInterface;
  */
 class StreamClient extends AbstractClient
 {
-    /**
-     * Any implementing HTTP providers should send a request to the provided endpoint with the parameters.
-     * They should return, in string form, the response body and throw an exception on error.
-     *
-     * @param UriInterface $endpoint
-     * @param mixed        $requestBody
-     * @param array        $extraHeaders
-     * @param string       $method
-     *
-     * @return string
-     *
-     * @throws TokenResponseException
-     * @throws \InvalidArgumentException
-     */
-    public function retrieveResponse(
-        UriInterface $endpoint,
-        $requestBody,
-        array $extraHeaders = array(),
-        $method = 'POST'
-    ) {
-        // Normalize method name
-        $method = strtoupper($method);
+  /**
+   * Any implementing HTTP providers should send a request to the provided endpoint with the parameters.
+   * They should return, in string form, the response body and throw an exception on error.
+   *
+   * @param UriInterface $endpoint
+   * @param mixed        $requestBody
+   * @param array        $extraHeaders
+   * @param string       $method
+   *
+   * @return string
+   *
+   * @throws TokenResponseException
+   * @throws \InvalidArgumentException
+   */
+  public function retrieveResponse(
+    UriInterface $endpoint,
+    $requestBody,
+    array $extraHeaders = array(),
+    $method = 'POST'
+  ) {
+    // Normalize method name
+    $method = strtoupper($method);
 
-        $this->normalizeHeaders($extraHeaders);
+    $this->normalizeHeaders($extraHeaders);
 
-        if ($method === 'GET' && !empty($requestBody)) {
-            throw new \InvalidArgumentException('No body expected for "GET" request.');
-        }
-
-        if (!isset($extraHeaders['Content-Type']) && $method === 'POST' && is_array($requestBody)) {
-            $extraHeaders['Content-Type'] = 'Content-Type: application/x-www-form-urlencoded';
-        }
-
-        $host = 'Host: '.$endpoint->getHost();
-        // Append port to Host if it has been specified
-        if ($endpoint->hasExplicitPortSpecified()) {
-            $host .= ':'.$endpoint->getPort();
-        }
-
-        $extraHeaders['Host']       = $host;
-        $extraHeaders['Connection'] = 'Connection: close';
-
-        if (is_array($requestBody)) {
-            $requestBody = http_build_query($requestBody, '', '&');
-        }
-        $extraHeaders['Content-length'] = 'Content-length: '.strlen($requestBody);
-
-        $context = $this->generateStreamContext($requestBody, $extraHeaders, $method);
-
-        $level = error_reporting(0);
-        $response = $this->getContents($endpoint->getAbsoluteUri());
-        error_reporting($level);
-        if (false === $response) {
-            $lastError = error_get_last();
-            if (is_null($lastError)) {
-                throw new TokenResponseException('Failed to request resource.');
-            }
-            throw new TokenResponseException($lastError['message']);
-        }
-
-        return $response;
+    if ($method === 'GET' && !empty($requestBody)) {
+      throw new \InvalidArgumentException('No body expected for "GET" request.');
     }
 
-    private function generateStreamContext($body, $headers, $method)
-    {
-        return stream_context_create(
-            array(
-                'http' => array(
-                    'method'           => $method,
-                    'header'           => implode("\r\n", array_values($headers)),
-                    'content'          => $body,
-                    'protocol_version' => '1.1',
-                    'user_agent'       => $this->userAgent,
-                    'max_redirects'    => $this->maxRedirects,
-                    'timeout'          => $this->timeout
-                ),
-            )
-        );
+    if (!isset($extraHeaders['Content-Type']) && $method === 'POST' && is_array($requestBody)) {
+      $extraHeaders['Content-Type'] = 'Content-Type: application/x-www-form-urlencoded';
     }
 
-    private function getContents($url) {
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        $data = curl_exec($ch);
-        curl_close($ch);
-
-        return $data;
+    $host = 'Host: '.$endpoint->getHost();
+    // Append port to Host if it has been specified
+    if ($endpoint->hasExplicitPortSpecified()) {
+      $host .= ':'.$endpoint->getPort();
     }
+
+    $extraHeaders['Host']       = $host;
+    $extraHeaders['Connection'] = 'Connection: close';
+
+    if (is_array($requestBody)) {
+      $requestBody = http_build_query($requestBody, '', '&');
+    }
+    $extraHeaders['Content-length'] = 'Content-length: '.strlen($requestBody);
+
+    $level = error_reporting(0);
+    $response = $this->getContents($endpoint->getAbsoluteUri(), $requestBody, $extraHeaders, $method);
+
+    error_reporting($level);
+    if (false === $response) {
+      $lastError = error_get_last();
+      if (is_null($lastError)) {
+        throw new TokenResponseException('Failed to request resource.');
+      }
+      throw new TokenResponseException($lastError['message']);
+    }
+
+    return $response;
+  }
+
+  private function getContents($url, $body = null, $headers = null, $method = 'GET') {
+    var_dump($url);
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
+    curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+
+    if ($body != null)
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+
+    if ($headers != null)
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    if ($method == 'POST')
+      curl_setopt($ch, CURLOPT_POST, 1);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+  }
 }
